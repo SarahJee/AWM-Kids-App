cd71b131dc24a26d698836a30e62cf26d118c5bd
Sarah Jee Watson <design@sarahjee.com>
2016-04-25 08:35:34 +0800

    JS

--- js/main.js
+++ js/main.js
@@ -58,3 +58,34 @@ $( ".secpurple" ).click(function() {
 $( ".secorange" ).click(function() {
   $( "#llmap" ).slideToggle( "slow" );
 });
+
+
+// http://codepen.io/robwakeman/pen/zxjGWX
+
+var answers = ["a","b","a","b","a","b","a","b","c"], 
+    tot = answers.length;
+
+function getCheckedValue( radioName ){
+    var radios = document.getElementsByName( radioName ); // Get radio group by-name
+    for(var y=0; y<radios.length; y++)
+      if(radios[y].checked) return radios[y].value; // return the checked value
+}
+
+function getScore(){
+  var score = 0;
+  for (var i=0; i<tot; i++)
+    if(getCheckedValue("question"+i)===answers[i]) score += 1; // increment only
+  return score;
+}
+
+function returnAnswer(){
+  alert("Your score is "+ getScore() +"/"+ tot);
+  }
+
+function returnScore(){
+  document.getElementById("myresults").innerHTML = "Your score is "+ getScore() +"/"+ tot;
+}
+
+
+
+
