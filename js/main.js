ce3bf1e91c4383b933091944b34ee231430668dd
Sarah Jee Watson <design@sarahjee.com>
2016-04-25 11:58:21 +0800

    Added answers..
    ..and changed classes.

--- js/main.js
+++ js/main.js
@@ -33,59 +33,115 @@ $('a.doSomething').click(function(){
 
 
 // Show hide Aircraft Hall Section
-$( ".secblue" ).click(function() {
+$( ".ah-blue" ).click(function() {
  $( "#aircrafthall" ).slideToggle( "slow" );
+});
+
+// Show hide First WW Section
+$( ".fww-green" ).click(function() {
+ $( "#firstworldwar" ).slideToggle( "slow" );
+});
+
+// Show hide Second WW Section
+$( ".sww-purple" ).click(function() {
+ $( "#secondworldwar" ).slideToggle( "slow" );
+});
+
+// Show hide Hall of V Section
+$( ".hov-orange" ).click(function() {
+ $( "#hallofvalour" ).slideToggle( "slow" );
+});
+
+// Show hide Anzac Hall Section
+$( ".ah-pink" ).click(function() {
+ $( "#anzachall" ).slideToggle( "slow" );
+});
+
+// Show hide Hall of M Section
+$( ".hom-blue" ).click(function() {
+ $( "#hallofmemory" ).slideToggle( "slow" );
+});
+
+// Show hide Conflicts Section
+$( ".con-green" ).click(function() {
+ $( "#conflicts45today" ).slideToggle( "slow" );
+});
+
+// Show hide Memorial Gds Section
+$( ".mg-purple" ).click(function() {
+ $( "memorialgrounds" ).slideToggle( "slow" );
+});
+
+// Show hide Sculpture Gdn Section
+$( ".sg-orange" ).click(function() {
+ $( "#sculpturegarden" ).slideToggle( "slow" );
 });
 
 
 
+
+
 // Show hide Maps
-$( ".secblue" ).click(function() {
+$( ".map-blue" ).click(function() {
   $( "#mgmap" ).slideToggle( "slow" );
 });
 
 // Show hide Maps
-$( ".secgreen" ).click(function() {
+$( ".map-green" ).click(function() {
   $( "#glmap" ).slideToggle( "slow" );
 });
 
 // Show hide Maps
-$( ".secpurple" ).click(function() {
+$( ".map-purple" ).click(function() {
   $( "#ufmap" ).slideToggle( "slow" );
 });
 
 // Show hide Maps
-$( ".secorange" ).click(function() {
+$( ".map-orange" ).click(function() {
   $( "#llmap" ).slideToggle( "slow" );
 });
 
 
 // http://codepen.io/robwakeman/pen/zxjGWX
 
-var answers = ["a","b","a","b","a","b","a","b","c"], 
+
+//Put answers in an array and store in 'Answers' variable.
+var answers = ["a","a","b","c","b","a","b","a","c"], 
+//Total # of possible answers
     tot = answers.length;
 
+
 function getCheckedValue( radioName ){
-    var radios = document.getElementsByName( radioName ); // Get radio group by-name
-    for(var y=0; y<radios.length; y++)
-      if(radios[y].checked) return radios[y].value; // return the checked value
+    var radios = document.getElementsByName( radioName ); //Find the answer choices for each question
+    for(var y=0; y<radios.length; y++) //Go to next q while one exists
+      if(radios[y].checked) return radios[y].value; // return the checked value ie the chosen answer
 }
 
-function getScore(){
-  var score = 0;
-  for (var i=0; i<tot; i++)
-    if(getCheckedValue("question"+i)===answers[i]) score += 1; // increment only
-  return score;
+
+function checkAnswer( radioName ){
+	if (radios[y].value === answers[y]) {
+		alert("Correct");
+	} else {
+		var response = document.getElementsByClassName( correctAnswer );
+		alert (response);
+	}
 }
 
 function returnAnswer(){
   alert("Your score is "+ getScore() +"/"+ tot);
   }
 
+
+// return total score 
+function getScore(){
+  var score = 0;
+  for (var i=0; i<tot; i++)
+    if(getCheckedValue("answer"+i)===answers[i]) score += 1; // add one when answer is correct
+  return score; // Store score in 'getScore'
+}
+
 function returnScore(){
   document.getElementById("myresults").innerHTML = "Your score is "+ getScore() +"/"+ tot;
 }
-
-
 
 
