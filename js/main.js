4428d81d2a55333d9003b98d88160fd0a2221fb2
Sarah Jee Watson <design@sarahjee.com>
2016-04-25 13:40:38 +0800

    JS updates
    Made Map & Disc Toggle DRY
    Trials for quiz options..

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
+/*
 $(document); // Activate jQuery for object
 $('#mydiv')  // Element with ID "mydiv"
 $('p.first') // P tags with class first.
@@ -11,7 +12,7 @@ $('a.doSomething').click(function(){
 	// Do something here!
 	alert('You did something, woo hoo!');
 });
-
+*/
 
     // Get the homepage div element 
 	//var home = document.getElementById( "homepage" );
@@ -30,118 +31,71 @@ $('a.doSomething').click(function(){
 //});
 
 
-
-
-// Show hide Aircraft Hall Section
-$( ".ah-blue" ).click(function() {
- $( "#aircrafthall" ).slideToggle( "slow" );
-});
-
-// Show hide First WW Section
-$( ".fww-green" ).click(function() {
- $( "#firstworldwar" ).slideToggle( "slow" );
-});
-
-// Show hide Second WW Section
-$( ".sww-purple" ).click(function() {
- $( "#secondworldwar" ).slideToggle( "slow" );
-});
-
-// Show hide Hall of V Section
-$( ".hov-orange" ).click(function() {
- $( "#hallofvalour" ).slideToggle( "slow" );
-});
-
-// Show hide Anzac Hall Section
-$( ".ah-pink" ).click(function() {
- $( "#anzachall" ).slideToggle( "slow" );
-});
-
-// Show hide Hall of M Section
-$( ".hom-blue" ).click(function() {
- $( "#hallofmemory" ).slideToggle( "slow" );
-});
-
-// Show hide Conflicts Section
-$( ".con-green" ).click(function() {
- $( "#conflicts45today" ).slideToggle( "slow" );
-});
-
-// Show hide Memorial Gds Section
-$( ".mg-purple" ).click(function() {
- $( "memorialgrounds" ).slideToggle( "slow" );
-});
-
-// Show hide Sculpture Gdn Section
-$( ".sg-orange" ).click(function() {
- $( "#sculpturegarden" ).slideToggle( "slow" );
+// Toggle map images on click. 
+$('.map[data-target]').click(function() {
+    var target = $(this).data('target');
+    $('#' + target).slideToggle('slow');
 });
 
 
 
 
-
-// Show hide Maps
-$( ".map-blue" ).click(function() {
-  $( "#mgmap" ).slideToggle( "slow" );
+// Toggle question sections on click. 
+$('.disc-list[data-target]').click(function() {
+    var target = $(this).data('target');
+    $('#' + target).slideToggle('slow');
 });
 
-// Show hide Maps
-$( ".map-green" ).click(function() {
-  $( "#glmap" ).slideToggle( "slow" );
-});
 
-// Show hide Maps
-$( ".map-purple" ).click(function() {
-  $( "#ufmap" ).slideToggle( "slow" );
-});
 
-// Show hide Maps
-$( ".map-orange" ).click(function() {
-  $( "#llmap" ).slideToggle( "slow" );
-});
+
 
 
 // http://codepen.io/robwakeman/pen/zxjGWX
 
 
 //Put answers in an array and store in 'Answers' variable.
-var answers = ["a","a","b","c","b","a","b","a","c"], 
+var answers = ["a","a","b","c","b","a","b","a","c"];
 //Total # of possible answers
-    tot = answers.length;
+    total = answers.length;
+    
+var checkedValue = getCheckedValue(  );
 
 
+//Create a function to store the users answer choice input (correct or not)
 function getCheckedValue( radioName ){
-    var radios = document.getElementsByName( radioName ); //Find the answer choices for each question
-    for(var y=0; y<radios.length; y++) //Go to next q while one exists
-      if(radios[y].checked) return radios[y].value; // return the checked value ie the chosen answer
+ 	var radios = document.getElementsByName( radioName ); //Find the answer choices for each question
+    for(var y=0; y<radios.length; y++) //Loop through answers to find checked?
+      if(radios[y].checked) return radios[y].value; // return the checked value ie the chosen answer/input
+
+
+    //Create a function to display answer and if correct or not
+		function checkAnswer(){
+			if (checkedValue === answers[y]) {
+				alert("Correct");
+			} else {
+				//var response = document.getElementsByClassName( answerConfirm );
+				alert ("Incorrect");
+			}
+}
 }
 
-
-function checkAnswer( radioName ){
-	if (radios[y].value === answers[y]) {
-		alert("Correct");
-	} else {
-		var response = document.getElementsByClassName( correctAnswer );
-		alert (response);
-	}
-}
-
+//========================This section is to display at the very end when all 9 questions have been answered=================//
 function returnAnswer(){
-  alert("Your score is "+ getScore() +"/"+ tot);
+  alert("Your score is "+ getScore() +"/"+ total);
   }
 
 
 // return total score 
 function getScore(){
   var score = 0;
-  for (var i=0; i<tot; i++)
+  for (var i=0; i<total; i++)
     if(getCheckedValue("answer"+i)===answers[i]) score += 1; // add one when answer is correct
   return score; // Store score in 'getScore'
 }
 
 function returnScore(){
-  document.getElementById("myresults").innerHTML = "Your score is "+ getScore() +"/"+ tot;
+  document.getElementById("myresults").innerHTML = "Your score is "+ getScore() +"/"+ total;
 }
 
 
