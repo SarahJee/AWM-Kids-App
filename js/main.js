cbe4f272cadefa7903bcca2d61ccc1ad57bd9151
Sarah Jee Watson <design@sarahjee.com>
2016-04-28 18:51:35 +0800

    .

--- js/main.js
+++ js/main.js
@@ -92,14 +92,8 @@ function getCheckedValue( question, radioName ){
 
     // display answer and if correct or not
 			if (checkedValue === answers[question]) {
-				alertswal({
-				  title: 'Sweet!',
-				  text: 'Modal with a custom image.',
-				  imageUrl: 'https://unsplash.it/400/200',
-				  imageWidth: 400,
-				  imageHeight: 200,
-				  animation: false
-			});
+				alert("Correct");
+				score++
 			} else {
 				alert ("Incorrect");
 			}
