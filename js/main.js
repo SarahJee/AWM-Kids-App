cd3cbcf8a57bfaf3d566f87c5f811e9bdc8bc823
Sarah Jee Watson <design@sarahjee.com>
2016-04-28 18:50:57 +0800

    Added Sweet Alert JS

--- js/main.js
+++ js/main.js
@@ -92,8 +92,14 @@ function getCheckedValue( question, radioName ){
 
     // display answer and if correct or not
 			if (checkedValue === answers[question]) {
-				alert("Correct");
-				score++
+				alertswal({
+				  title: 'Sweet!',
+				  text: 'Modal with a custom image.',
+				  imageUrl: 'https://unsplash.it/400/200',
+				  imageWidth: 400,
+				  imageHeight: 200,
+				  animation: false
+			});
 			} else {
 				alert ("Incorrect");
 			}
